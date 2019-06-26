97883f4160bdeec8d296d4075b7518f5f09dbee5
Pawel Tarsala (PLT) <PLT@kmd.dk>
2019-06-26 06:40:12 +0200

    little cleanup

--- RuntimeCompilerApi/Controllers/CompilerController.cs
+++ RuntimeCompilerApi/Controllers/CompilerController.cs
@@ -21,84 +21,16 @@ namespace RuntimeCompilerApi.Controllers
         [HttpPost]
         public IActionResult Post([FromBody] CompileCodeRequest codeRequest)
         {
-            string path = "compiledFile.exe";
-            try
-            {
-                Sth();
-            }
-            catch (Exception)
-            {
-                Console.WriteLine("Error during file compilation");
-                throw;
-            }
-            return Ok($"File has been successfully compiled. Check [SolutionDirectory]/bin/Debug/{path}");
-        }
-        [HttpGet]
-        public IActionResult Get()
-        {
-            string path = "testcode.txt";
-            string resultFilePath = "result.exe";
-            string code = System.IO.File.ReadAllText(path);
-            //CompilerService.CompileRoslyn(code, resultFilePath);
-            //CompilerService.CompileRoslyn2(code, resultFilePath);
-            object result = CompilerService.Roslyn3(path);
+            object result = CompilerService.Compile(codeRequest.Code);
 
             return Ok(result);
         }
-        private void Sth()
+        [HttpGet]
+        public IActionResult Get(string codeFile, string resultFileName)
         {
-            string codeToCompile = @"
-            using System;
-            namespace RoslynCompileSample
-            {
-                public class Writer
-                {
-                    public void Write(string message)
-                    {
-                        Console.WriteLine($""you said '{message}!'"");
-                    }
-                }
-            }";
+            object result = CompilerService.Compile(codeFile, resultFileName);
 
-            SyntaxTree syntaxTree = CSharpSyntaxTree.ParseText(codeToCompile);
-
-            string assemblyName = Path.GetRandomFileName();
-            MetadataReference[] references = new MetadataReference[]
-            {
-                MetadataReference.CreateFromFile(typeof(object).GetTypeInfo().Assembly.Location)
-            };
-
-            CSharpCompilation compilation = CSharpCompilation.Create(
-                assemblyName,
-                syntaxTrees: new[] { syntaxTree },
-                references: references,
-                options: new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary));
-
-            using (var ms = new MemoryStream())
-            {
-                EmitResult result = compilation.Emit(ms);
-
-                if (!result.Success)
-                {
-                    IEnumerable<Diagnostic> failures = result.Diagnostics.Where(diagnostic =>
-                        diagnostic.IsWarningAsError ||
-                        diagnostic.Severity == DiagnosticSeverity.Error);
-
-                    foreach (Diagnostic diagnostic in failures)
-                    {
-                        Console.Error.WriteLine("\t{0}: {1}", diagnostic.Id, diagnostic.GetMessage());
-                    }
-                }
-                else
-                {
-                    ms.Seek(0, SeekOrigin.Begin);
-                    Assembly assembly = AssemblyLoadContext.Default.LoadFromStream(ms);
-                    var type = assembly.GetType("RoslynCompileSample.Writer");
-                    var instance = assembly.CreateInstance("RoslynCompileSample.Writer");
-                    var meth = type.GetMember("Write").First() as MethodInfo;
-                    meth.Invoke(instance, new[] { "joel" });
-                }
-            }
+            return Ok(result);
         }
     }
 }
--- RuntimeCompilerApi/Services/CompilerService.cs
+++ RuntimeCompilerApi/Services/CompilerService.cs
@@ -16,143 +16,19 @@ namespace RuntimeCompilerApi.Services
 {
     public static class CompilerService
     {
-        private static readonly string path = "testcode.cs";
-        public static void CompileCSharp(string code, string path)
+        public static object Compile(string fileToCompile, string resultFileName)
         {
-            try
-            {
-                var csc = new CSharpCodeProvider(new Dictionary<string, string>() { { "CompilerVersion", "v3.5" } });
-                var parameters = new CompilerParameters(new[] { "mscorlib.dll", "System.Core.dll" }, path, true)
-                {
-                    GenerateExecutable = true
-                };
-                CompilerResults results = csc.CompileAssemblyFromSource(parameters,code);
-                if (results.Errors.HasErrors)
-                {
-                    results.Errors.Cast<CompilerError>().ToList().ForEach(error => Console.WriteLine(error.ErrorText));
-                }
-            }
-            catch (Exception compilerException)
-            {
-                throw new CompilerException("Compiler failed to compiler the file",
-                                            compilerException);
-            }
-
-        }
-
-        public static void CompileRoslyn(string code, string fileName)
-        {
-            SyntaxTree syntaxTree = CSharpSyntaxTree.ParseText(code);
-
-            string assemblyName = Path.GetRandomFileName();
-            MetadataReference[] references = new MetadataReference[]
-            {
-                MetadataReference.CreateFromFile(typeof(object).GetTypeInfo().Assembly.Location)
-            };
-
-            CSharpCompilation compilation = CSharpCompilation.Create(
-                assemblyName,
-                syntaxTrees: new[] { syntaxTree },
-                references: references,
-                options: new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary));
-
-            using (var ms = File.Create(fileName))
-            {
-                EmitResult result = compilation.Emit(ms);
-
-                if (!result.Success)
-                {
-                    IEnumerable<Diagnostic> failures = result.Diagnostics.Where(diagnostic =>
-                        diagnostic.IsWarningAsError ||
-                        diagnostic.Severity == DiagnosticSeverity.Error);
-
-                    foreach (Diagnostic diagnostic in failures)
-                    {
-                        Console.Error.WriteLine("\t{0}: {1}", diagnostic.Id, diagnostic.GetMessage());
-                    }
-                }
-                else
-                {
-                    ms.Seek(0, SeekOrigin.Begin);
-                    Assembly assembly = AssemblyLoadContext.Default.LoadFromStream(ms);
-                    var type = assembly.GetType("RoslynCompileSample.Writer");
-                    var instance = assembly.CreateInstance("RoslynCompileSample.Writer");
-                    var meth = type.GetMember("Write").First() as MethodInfo;
-                    meth.Invoke(instance, new[] { "joel" });
-                }
-            }
-        }
-
-        public static void CompileRoslyn2(string code, string fileName)
-        {
-            // intentionally introduce errors to see the compiler reporting code issues
-            code = @"using System; 
-                using System.IO; 
-                namespace MathFunctions 
-                { 
-                 public static class MathHelper
-                 { 
-                    public static void CalculateCircleArea() 
-                    { 
-                        double radius = 10;
-                        double result = radius * radius * System.Math.PI;
-                        Console.WriteLine(result.ToString()); 
-                    } 
-                  } 
-                }";
-
-            code = File.ReadAllText("testcode2.txt");
-
-            // Get a SyntaxTree
-            var tree = SyntaxFactory.ParseSyntaxTree(code);
-
-            Console.WriteLine(tree);
-            PrintDiagnostics(tree);
-
-            // Create a compilation for the syntax tree
-            var compilation = CSharpCompilation.Create("mylib.dll").
-                WithOptions(
-                    new CSharpCompilationOptions(OutputKind.ConsoleApplication)).
-                AddReferences(MetadataReference.CreateFromFile(typeof(object).
-                GetTypeInfo().Assembly.Location)).
-                AddSyntaxTrees(tree);
-
-            fileName = "mylib.dll";
-            var path = Path.Combine(Directory.GetCurrentDirectory(), fileName);
-
-            // Emit an Assembly that contains the result of the Roslyn code generation
-            compilation.Emit(path);
-
-           var asm = AssemblyLoadContext.Default.LoadFromAssemblyPath(path);
-            asm.GetType("MathFunctions.MathHelper").GetMethod("CalculateCircleArea").Invoke(null, new object[] { });
-            Console.ReadLine();
-        }
-        private static void PrintDiagnostics(SyntaxTree tree)
-        {
-            // detects diagnostics in the source code
-            var diagnostics = tree.GetDiagnostics();
-
-            if (diagnostics.Any())
-            {
-                foreach (var diag in diagnostics)
-                {
-                    // if any, prints diagnostic message and line/row position
-                    Console.WriteLine($"{diag.GetMessage()} {diag.Location.GetLineSpan()}");
-                }
-            }
-        }
-
-        public static object Roslyn3(string path)
-        {
-            var fileToCompile = "testcode.txt";
             var source = File.ReadAllText(fileToCompile);
             var parsedSyntaxTree = Parse(source, "", CSharpParseOptions.Default.WithLanguageVersion(LanguageVersion.CSharp5));
 
+
             var compilation
-                = CSharpCompilation.Create("Test.dll", new SyntaxTree[] { parsedSyntaxTree }, DefaultReferences, DefaultCompilationOptions);
+                = CSharpCompilation.Create(resultFileName, new SyntaxTree[] { parsedSyntaxTree }, DefaultReferences, DefaultCompilationOptions);
+            string projectPath = @"C:\Users\Z6PLT\source\repos\MyProjects\Test_Projects\RuntimeCompilerApi\";
+            string fullPath = projectPath + resultFileName;
             try
             {
-                var result = compilation.Emit(@"C:\Users\Z6PLT\source\repos\MyProjects\Test_Projects\RuntimeCompilerApi\Test.dll");
+                var result = compilation.Emit(fullPath);
 
                 Console.WriteLine(result.Success ? "Sucess!!" : "Failed");
             }
@@ -160,7 +36,28 @@ namespace RuntimeCompilerApi.Services
             {
                 Console.WriteLine(ex);
             }
-            return InvokeDll(@"C:\Users\Z6PLT\source\repos\MyProjects\Test_Projects\RuntimeCompilerApi\Test.dll");
+            return InvokeDll(fullPath);
+        }
+        public static object Compile(string code)
+        {
+            var parsedSyntaxTree = Parse(code, "", CSharpParseOptions.Default.WithLanguageVersion(LanguageVersion.CSharp5));
+            var resultFileName = "Test" + DateTime.Now.Millisecond + ".dll";
+
+            var compilation
+                = CSharpCompilation.Create(resultFileName, new SyntaxTree[] { parsedSyntaxTree }, DefaultReferences, DefaultCompilationOptions);
+            string projectPath = @"C:\Users\Z6PLT\source\repos\MyProjects\Test_Projects\RuntimeCompilerApi\";
+            string fullPath = projectPath + resultFileName;
+            try
+            {
+                var result = compilation.Emit(fullPath);
+
+                Console.WriteLine(result.Success ? "Sucess!!" : "Failed");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+            return InvokeDll(fullPath);
         }
         private static readonly IEnumerable<string> DefaultNamespaces =
             new[]
